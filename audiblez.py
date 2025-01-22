941e3e39b6349ef13bf3f64de75ca7205e949422
tom metge <tom@accident-prone.com>
2025-01-22 08:23:51 -0800

    Improve formatting (according to pylint)

--- audiblez.py
+++ audiblez.py
@@ -4,22 +4,22 @@
 # by Claudio Santini 2025 - https://claudio.uk
 
 import argparse
-import sys
-import time
+import re
 import shutil
 import subprocess
-import soundfile as sf
-import ebooklib
+import sys
+import time
 import warnings
-import re
 from pathlib import Path
 from string import Formatter
+import soundfile as sf
+import ebooklib
 from bs4 import BeautifulSoup
-from kokoro_onnx import Kokoro
 from ebooklib import epub
-from pydub import AudioSegment
-from pick import pick
+from kokoro_onnx import Kokoro
 import onnxruntime as ort
+from pick import pick
+from pydub import AudioSegment
 
 
 def main(kokoro, file_path, lang, voice, pick_manually, speed, providers):
@@ -33,7 +33,7 @@ def main(kokoro, file_path, lang, voice, pick_manually, speed, providers):
             sys.exit(1)
         kokoro.sess.set_providers(providers)
         print(f"Using ONNX providers: {', '.join(providers)}")
-    
+
     filename = Path(file_path).name
     with warnings.catch_warnings():
         book = epub.read_epub(file_path)
@@ -41,7 +41,8 @@ def main(kokoro, file_path, lang, voice, pick_manually, speed, providers):
     creator = book.get_metadata('DC', 'creator')[0][0]
     intro = f'{title} by {creator}'
     print(intro)
-    print('Found Chapters:', [c.get_name() for c in book.get_items() if c.get_type() == ebooklib.ITEM_DOCUMENT])
+    print('Found Chapters:',
+        [c.get_name() for c in book.get_items() if c.get_type() == ebooklib.ITEM_DOCUMENT])
     if pick_manually:
         chapters = pick_chapters(book)
     else:
@@ -50,8 +51,10 @@ def main(kokoro, file_path, lang, voice, pick_manually, speed, providers):
     texts = extract_texts(chapters)
     has_ffmpeg = shutil.which('ffmpeg') is not None
     if not has_ffmpeg:
-        print('\033[91m' + 'ffmpeg not found. Please install ffmpeg to create mp3 and m4b audiobook files.' + '\033[0m')
-    total_chars = sum([len(t) for t in texts])
+        print('\033[91m' +
+            'ffmpeg not found. Please install ffmpeg to create mp3 and m4b audiobook files.' +
+            '\033[0m')
+    total_chars = sum(len(t) for t in texts)
     print('Started at:', time.strftime('%H:%M:%S'))
     print(f'Total characters: {total_chars:,}')
     print('Total words:', len(' '.join(texts).split(' ')))
@@ -80,11 +83,12 @@ def main(kokoro, file_path, lang, voice, pick_manually, speed, providers):
         end_time = time.time()
         delta_seconds = end_time - start_time
         chars_per_sec = len(text) / delta_seconds
-        remaining_chars = sum([len(t) for t in texts[i - 1:]])
+        remaining_chars = sum(len(t) for t in texts[i - 1:])
         remaining_time = remaining_chars / chars_per_sec
         print(f'Estimated time remaining: {strfdelta(remaining_time)}')
         print('Chapter written to', chapter_filename)
-        print(f'Chapter {i} read in {delta_seconds:.2f} seconds ({chars_per_sec:.0f} characters per second)')
+        print(f'Chapter {i} read in {delta_seconds:.2f} seconds ({chars_per_sec:.0f}' +
+            ' characters per second)')
         progress = int((total_chars - remaining_chars) / total_chars * 100)
         print('Progress:', f'{progress}%')
         i += 1
@@ -108,6 +112,8 @@ def extract_texts(chapters):
 
 
 def is_chapter(c):
+    if c.get_type() != ebooklib.ITEM_DOCUMENT:
+        return False
     name = c.get_name().lower()
     part = r"part\d{1,3}"
     if re.search(part, name):
@@ -120,15 +126,17 @@ def is_chapter(c):
         return True
     if 'chapter' in name:
         return True
+    return False
 
 
 def find_chapters(book, verbose=False):
-    chapters = [c for c in book.get_items() if c.get_type() == ebooklib.ITEM_DOCUMENT and is_chapter(c)]
+    chapters = [c for c in book.get_items() if is_chapter(c)]
     if verbose:
         for item in book.get_items():
             if item.get_type() == ebooklib.ITEM_DOCUMENT:
                 print(f"'{item.get_name()}'" + ', #' + str(len(item.get_body_content())))
-                # print(f'{item.get_name()}'.ljust(60), str(len(item.get_body_content())).ljust(15), 'X' if item in chapters else '-')
+                #print(f'{item.get_name()}'.ljust(60), str(len(item.get_body_content())).ljust(15),
+                #    'X' if item in chapters else '-')
     if len(chapters) == 0:
         print('Not easy to find the chapters, defaulting to all available documents.')
         chapters = [c for c in book.get_items() if c.get_type() == ebooklib.ITEM_DOCUMENT]
@@ -136,9 +144,12 @@ def find_chapters(book, verbose=False):
 
 
 def pick_chapters(book):
-    all_chapters_names = [c.get_name() for c in book.get_items() if c.get_type() == ebooklib.ITEM_DOCUMENT]
+    all_chapters_names = [
+        c.get_name() for c in book.get_items() if c.get_type() == ebooklib.ITEM_DOCUMENT
+    ]
     title = 'Select which chapters to read in the audiobook'
-    selected_chapters_names = pick(all_chapters_names, title, multiselect=True, min_selection_count=1)
+    selected_chapters_names = pick(
+        all_chapters_names, title, multiselect=True, min_selection_count=1)
     selected_chapters_names = [c[0] for c in selected_chapters_names]
     selected_chapters = [c for c in book.get_items() if c.get_name() in selected_chapters_names]
     return selected_chapters
@@ -172,8 +183,8 @@ def create_m4b(chapter_files, filename, title, author):
         'ffmpeg', '-i', f'{tmp_filename}', '-c', 'copy', '-f', 'mp4',
         '-metadata', f'title={title}',
         '-metadata', f'author={author}',
-        f'{final_filename}'
-    ])
+        f'{final_filename}',
+    ], check=False)
     Path(tmp_filename).unlink()
     if proc.returncode == 0:
         print(f'{final_filename} created. Enjoy your audiobook.')
@@ -182,9 +193,11 @@ def create_m4b(chapter_files, filename, title, author):
 
 def cli_main():
     if not Path('kokoro-v0_19.onnx').exists() or not Path('voices.json').exists():
-        print('Error: kokoro-v0_19.onnx and voices.json must be in the current directory. Please download them with:')
-        print('wget https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files/kokoro-v0_19.onnx')
-        print('wget https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files/voices.json')
+        kokoro_base = "https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files"
+        print('Error: kokoro-v0_19.onnx and voices.json must be in the current directory. ' +
+            'Please download them with:')
+        print('wget ' + kokoro_base + '/kokoro-v0_19.onnx')
+        print('wget ' + kokoro_base + '/voices.json')
         sys.exit(1)
     kokoro = Kokoro('kokoro-v0_19.onnx', 'voices.json')
     voices = list(kokoro.get_voices())
@@ -197,20 +210,34 @@ def cli_main():
     available_providers = ort.get_available_providers()
     providers_help = f"Available ONNX providers: {', '.join(available_providers)}"
 
-    parser = argparse.ArgumentParser(epilog=epilog, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser = argparse.ArgumentParser(
+        epilog=epilog,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('epub_file_path', help='Path to the epub file')
-    parser.add_argument('-l', '--lang', default='en-gb', help='Language code: en-gb, en-us, fr-fr, ja, ko, cmn')
-    parser.add_argument('-v', '--voice', default=default_voice, help=f'Choose narrating voice: {voices_str}')
-    parser.add_argument('-p', '--pick', default=False, help=f'Interactively select which chapters to read in the audiobook',
-                        action='store_true')
-    parser.add_argument('-s', '--speed', default=1.0, help=f'Set speed from 0.5 to 2.0', type=float)
-    parser.add_argument('--providers', nargs='+', metavar='PROVIDER', help=f"Specify ONNX providers. {providers_help}")
-    
+    parser.add_argument('-l', '--lang',
+        default='en-gb',
+        help='Language code: en-gb, en-us, fr-fr, ja, ko, cmn')
+    parser.add_argument('-v', '--voice',
+        default=default_voice,
+        help=f'Choose narrating voice: {voices_str}')
+    parser.add_argument('-p', '--pick',
+        default=False,
+        help='Interactively select which chapters to read in the audiobook',
+        action='store_true')
+    parser.add_argument('-s', '--speed',
+        default=1.0,
+        help='Set speed from 0.5 to 2.0',
+        type=float)
+    parser.add_argument('--providers',
+        nargs='+',
+        metavar='PROVIDER',
+        help=f"Specify ONNX providers. {providers_help}")
     if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         sys.exit(1)
     args = parser.parse_args()
-    main(kokoro, args.epub_file_path, args.lang, args.voice, args.pick, args.speed, args.providers)
+    main(kokoro, args.epub_file_path, args.lang, args.voice,
+        args.pick, args.speed, args.providers)
 
 
 if __name__ == '__main__':
